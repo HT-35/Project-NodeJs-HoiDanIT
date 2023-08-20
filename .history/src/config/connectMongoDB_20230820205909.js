24b56fc1565c061b79c62c57470c60e193e64df9
unknown <huyfa352002@gmail.com>
2023-08-21 02:42:16 +0700

    goon

--- .history/src/config/connectMongoDB_20230820205909.js
+++
@@ -1,7 +0,0 @@
-const require = createRequire(import.meta.url);
-
-const mongoose = require("mongoose");
-
-mongoose
-  .connect("mongodb://127.0.0.1:27017/test")
-  .then(() => console.log("Connected!"));
